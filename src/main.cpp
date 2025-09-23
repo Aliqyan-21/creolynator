68be9b1999ce7226f828ed89c4074ba4b2dc607f
Aliqyan-21 <aliqyanabid21@gmail.com>
2025-09-23 15:16:46 +0530

    patch: main function using ILexer too

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include "b_lexer.h"
-#include "i_lexer.h"
+#include "error.h"
 #include "iostream"
 #include "utils.h"
 
 int main(int argc, char *argv[]) {
   Args args = parse_args(argc, argv);
   try {
-    B_Lexer b_lexer(args.filename);
-    b_lexer.b_tokenize();
-    b_lexer.print_tokens();
-  } catch (const std::exception &e) {
-    std::cout << e.what() << std::endl;
+    BLexer blexer(args.filename);
+    blexer.b_tokenize();
+    blexer.process_inline_tokens();
+    blexer.print_tokens();
+  } catch (const CNError &e) {
+    std::cout << e.format() << std::endl;
   }
   return 0;
 }
