725f0836c4e1bf9d8cdf136bfe7fe30fb63b205d
Aliqyan-21 <aliqyanabid21@gmail.com>
2025-10-13 15:32:48 +0530

    add(UTILS): comments

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <iostream>
 
+/* prints usage info on console */
 void usage(const std::string &program) {
   std::cout << "Usage: " << program << " <input filepath>" << std::endl;
 }
 
+/* opens file and reads it's content into a string and returns it */
 std::string read_creole_file(const std::string &filepath) {
   std::ifstream infile(filepath);
   if (!infile) {
@@ -17,6 +19,7 @@ std::string read_creole_file(const std::string &filepath) {
   return content;
 }
 
+/* parses the cli arguments, stores them in Args struct and returns it */
 Args parse_args(int argc, char *argv[]) {
   if (argc < 2) {
     usage(argv[0]);
@@ -27,6 +30,7 @@ Args parse_args(int argc, char *argv[]) {
   return args;
 }
 
+/* trims whitespace from left direction of string and returns it */
 std::string ltrim(const std::string &str) {
   size_t i = 0;
   while (i < str.size() && std::isspace(str[i])) {
@@ -35,6 +39,7 @@ std::string ltrim(const std::string &str) {
   return str.substr(i);
 }
 
+/* trims whitespace from right direction of string and returns it */
 std::string rtrim(const std::string &str) {
   size_t j = str.size();
   while (j > 0 && std::isspace(str[j - 1])) {
@@ -43,4 +48,6 @@ std::string rtrim(const std::string &str) {
   return str.substr(0, j);
 }
 
+/* trims whitespace from left and right
+both direction of string and returns it */
 std::string trim(const std::string &str) { return ltrim(rtrim(str)); }
