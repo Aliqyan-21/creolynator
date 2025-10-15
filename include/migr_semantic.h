4661e607c583f61e5fcbb576b116a3fbd203b5a0
Aliqyan-21 <aliqyanabid21@gmail.com>
2025-10-15 18:15:58 +0530

    feat(SemanticLayer): semantic extraction, and query functions

--- include/migr_semantic.h
+++ include/migr_semantic.h
@@ -16,13 +16,25 @@ public:
   void serialize(std::ostream &out) const override;
   void deserialize(std::istream &in) const override;
 
+  /* Semantic Operations */
+  void extract_semantics(const StructuralLayer &structural);
+  void add_cross_reference(const std::string &from_id, const std::string &to_id,
+                           const std::string &relation_type);
+  std::vector<std::shared_ptr<MIGRNode>>
+  find_backlinks(const std::string &target_id);
+
 private:
   std::unordered_map<std::string, std::shared_ptr<MIGRNode>>
       semantic_nodes_; // [id : node]
   std::unordered_map<std::string, std::vector<std::string>>
       edges_; // [source : targets]
   std::unordered_map<std::string, std::vector<std::string>>
-      backlinks_; // [source : targets]
+      backlinks_; // [target : sources]
+
+  /* Helpers */
+  void extract_links(std::shared_ptr<MIGRNode> node);
+  void extract_tags(std::shared_ptr<MIGRNode> node);
+  void build_backlinks();
 };
 
 #endif //! MIGR_SEMANTIC_H
--- src/migr_semantic.cpp
+++ src/migr_semantic.cpp
@@ -36,7 +36,7 @@ void SemanticLayer::serialize(std::ostream &out) const {
   out << "    \"edges\": [\n";
 
   bool first{true};
-  for (const auto [source, targets] : edges_) {
+  for (const auto &[source, targets] : edges_) {
     for (const std::string &target : targets) {
       if (!first) {
         out << ",\n";
@@ -50,8 +50,65 @@ void SemanticLayer::serialize(std::ostream &out) const {
   out << "  }\n";
 }
 
-void StructuralLayer::deserialize(std::istream &in) const {
+void SemanticLayer::deserialize(std::istream &in) const {
   // todo: implement deserialization of data - would implement
   // json parsing as data will be in json format
   SPEAK << "Deserialization is not implemented yet" << std::endl;
+}
+
+void SemanticLayer::extract_semantics(const StructuralLayer &structural) {
+  auto root = structural.get_root();
+  if (root) {
+    extract_links(root);
+    extract_tags(root);
+    build_backlinks();
+  }
+}
+
+void SemanticLayer::add_cross_reference(const std::string &from_id,
+                                        const std::string &to_id,
+                                        const std::string &relation_type) {
+  edges_[from_id].push_back(to_id);
+  backlinks_[to_id].push_back(from_id);
+  // note: we can use relation type but don't know how?
+}
+
+std::vector<std::shared_ptr<MIGRNode>>
+SemanticLayer::find_backlinks(const std::string &target_id) {
+  std::vector<std::shared_ptr<MIGRNode>> results;
+  auto it = backlinks_.find(target_id);
+  if (it != backlinks_.end()) {
+    for (const std::string &bl_id : it->second) {
+      auto node_it = semantic_nodes_.find(bl_id);
+      if (node_it != semantic_nodes_.end()) {
+        results.push_back(node_it->second);
+      }
+    }
+  }
+  return results;
+}
+
+void SemanticLayer::extract_links(std::shared_ptr<MIGRNode> node) {
+  // todo: implement
+  // for now I can think of:
+  // - have to look for link patterns in content '[[link]]'.
+  // - then if found matches, then create a reference node.
+  // - and if node will have children then we can recursively link in them.
+}
+
+void SemanticLayer::extract_tags(std::shared_ptr<MIGRNode> node) {
+  // todo: implement
+  // for now I can think of:
+  // - have to look for tag patterns in content '#tag'.
+  // - then if found matches, then create a tag node.
+  // - and if node will have children then we can recursively link in them.
+}
+
+void SemanticLayer::build_backlinks() {
+  backlinks_.clear();
+  for (const auto &[source, targets] : edges_) {
+    for (const auto &target : targets) {
+      backlinks_[target].push_back(source);
+    }
+  }
 }
